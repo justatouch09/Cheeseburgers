3de3ef893341affba05a9da6e7aed03b3ca7241a
Jarad Touchberry <you@example.com>
2017-04-20 22:18:15 -0400

    code burgers

---
+++ src/com/company/Details.java
@@ -0,0 +1,20 @@
+package com.company;
+
+/**
+ * Created by jaradtouchberry on 4/17/17.
+ */
+public class Details {
+    String mayo;
+    int pattie;
+    int bun;
+    int bacon;
+    String instruction;
+
+    public Details(String mayo, int pattie, int bun, int bacon, String instruction) {
+        this.mayo = mayo;
+        this.pattie = pattie;
+        this.bun = bun;
+        this.bacon = bacon;
+        this.instruction = instruction;
+    }
+}
